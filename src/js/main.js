7c05e648d42a29a61918d5392d8df941eca81ba6
Tito <tawfik.bakr@gmail.com>
2022-07-25 08:20:27 +0100

    fixing a few issues

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('.slides-container').slick({
         autoplay:true,
         autoplaySpeed:5000,
-        infinite:true,
         adaptiveHeight: true,
         touchThreshold:100,
         dots:true,
         arrows:false,
+        lazyLoad: 'progressive',
         dotsClass:'slick-slider-dots',
         cssEase: 'ease'
     });
