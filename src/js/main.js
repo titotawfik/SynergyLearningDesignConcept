f8c6cc7d2f98782e8ff4c13c5e3c7c3cb6e4c808
Tito <tawfik.bakr@gmail.com>
2022-08-01 20:45:47 +0100

    fix indentation

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $('.slides-container').slick({
-        autoplay:true,
-        autoplaySpeed:5000,
-        adaptiveHeight: true,
-        touchThreshold:100,
-        dots:true,
-        arrows:false,
-        lazyLoad: 'progressive',
-        dotsClass:'slick-slider-dots',
-        cssEase: 'ease'
-    });
+  $('.slides-container').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    adaptiveHeight: true,
+    touchThreshold:100,
+    dots:true,
+    arrows:false,
+    lazyLoad: 'progressive',
+    dotsClass:'slick-slider-dots',
+    cssEase: 'ease'
+  });
 });
